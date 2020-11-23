08efdf59faa2060363abae90fc06621c9fc95a7c
Dunkelweizen <cai.nowicki@gmail.com>
2020-11-22 22:17:30 -0500

    trying to add right-hand travel rule

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -5,19 +5,36 @@ from world import World
 import random
 from ast import literal_eval
 
+
+class Queue():
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return len(self.queue)
+
+
 # Load world
 world = World()
 
-
 # You may uncomment the smaller graphs for development and testing purposes.
-#map_file = "maps/test_line.txt"
-#map_file = "maps/test_cross.txt"
-#map_file = "maps/test_loop.txt"
-map_file = "maps/test_loop_fork.txt"
-#map_file = "maps/main_maze.txt"
+# map_file = "maps/test_line.txt"
+# map_file = "maps/test_cross.txt"
+# map_file = "maps/test_loop.txt"
+# map_file = "maps/test_loop_fork.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
+room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
@@ -29,44 +46,76 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
-
-traversal_graph = {}
 room_init = {'n': '?', 's': '?', 'w': '?', 'e': '?'}
-opposite_dir_dict = {'n':'s', 's':'n', 'e':'w', 'w':'e'}
+traversal_graph = {0: room_init.copy()}
+try_order = ['e','s', 'w', 'n']
 
 def walk_graph(traversal_graph):
     dead_end = False
-
     while not dead_end:
         room = player.current_room.id
-        print(room)
         if room not in traversal_graph:
             traversal_graph[room] = room_init.copy()
-        try:
-            next_direction = random.choice([x for x in traversal_graph[room].keys() if traversal_graph[room][x] == '?'])
-            player.travel(next_direction)
-            new_room = player.current_room.id
-            if room != new_room:
-                traversal_graph[room][next_direction] = new_room
-                traversal_path.append(next_direction)
-            else:
-                traversal_graph[room][next_direction] = 'X'
-        except IndexError:
-            dead_end = True
+            moved = False
+            i = 0
+            while not moved and i < 4:
+                player.travel(try_order[i])
+                new_room = player.current_room.id
+                if room != new_room:
+                    traversal_graph[room][try_order[i]] = new_room
+                    traversal_path.append(try_order[i])
+                    moved = True
+                else:
+                    traversal_graph[room][try_order[i]] = room
+                i += 1
+        else:
+            try:
+                next_direction = random.choice(
+                    [x for x in traversal_graph[room].keys() if traversal_graph[room][x] == '?'])
+                player.travel(next_direction)
+                new_room = player.current_room.id
+                if room != new_room:
+                    traversal_graph[room][next_direction] = new_room
+                    traversal_path.append(next_direction)
+                else:
+                    traversal_graph[room][next_direction] = room
+            except IndexError:
+                dead_end = True
     return traversal_graph
 
-while len(traversal_graph) < world.grid_size:
+
+def bfs(current_room, graph):
+    visited = set()
+    queue = Queue()
+    queue.enqueue([current_room])
+    while queue.size() > 0:
+        current_path = queue.dequeue()
+        current_room = current_path[-1]
+        possible_exits = graph[current_room].values()
+        if '?' in possible_exits:
+            return current_path
+        if current_room not in visited:
+            visited.add(current_room)
+            exits = traversal_graph[current_room].values()
+            for pos_exit in exits:
+                path_copy = current_path + [pos_exit]
+                queue.enqueue(path_copy)
+
+
+while bfs(player.current_room.id, traversal_graph) is not None:
+    # graph does not yet contain all rooms
+    # player is standing in a room with no univisited exits
+    # need to search from here to next room with unvisited exit, then travel there
+    travel_rooms = bfs(player.current_room.id, traversal_graph)
+    for room in travel_rooms[1:]:
+        switched = [(value, key) for (key, value) in traversal_graph[player.current_room.id].items()]
+        for pair in switched:
+            if pair[0] == room:
+                player.travel(pair[1])
+                traversal_path.append(pair[1])
+                break
     walk_graph(traversal_graph)
-    i = -1
-    while '?' not in traversal_graph[player.current_room.id].values() and i > 0:
-        reverse = opposite_dir_dict[traversal_path[i]]
-        traversal_path.append(reverse)
-        player.travel(reverse)
-        i -= 1
 
-
-
-print(traversal_path)
 # TRAVERSAL TEST
 visited_rooms = set()
 player.current_room = world.starting_room
@@ -81,8 +130,6 @@ if len(visited_rooms) == len(room_graph):
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
-
-
 
 #######
 # UNCOMMENT TO WALK AROUND
